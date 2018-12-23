218e5ee150ad14bdf8ad38047718075bed8c816a
Mac Dowell Prosper <macdowell@noukod.com>
2018-12-23 06:17:38 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -40,7 +40,7 @@ class ApplicationController < Sinatra::Base
     recipe.ingredients = params[:ingredients]
     recipe.cook_time = params[:cook_time]
     recipe.save
-    redirect "/recipe/#{recipe.id}"
+    redirect "/recipes/#{recipe.id}"
   end
 
   delete '/recipes/:id/delete' do
