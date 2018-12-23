f7fc29d1f763810864fd3e648663be26f8ba9c08
Mac Dowell Prosper <macdowell@noukod.com>
2018-12-23 06:07:07 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,16 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
+  get '/recipes/new' do
+    erb :new
+  end
+
+  post '/recipes' do
+    recipe = Recipe.create name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time]
+
+    redirect "/recipes/#{recipe.id}"
+  end
+
   get '/recipes/:id' do
     @recipe = Recipe.find params[:id]
 
@@ -22,16 +32,6 @@ class ApplicationController < Sinatra::Base
     @recipe = Recipe.find params[:id]
 
     erb :edit
-  end
-
-  get '/recipes/new' do
-    erb :new
-  end
-
-  post '/recipes' do
-    recipe = Recipe.create name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time]
-
-    redirect "/recipes/#{recipe.id}"
   end
 
   delete '/recipes/:id/delete' do
