16e525abce1a95876a3df77e16166276dd37edb8
Mac Dowell Prosper <macdowell@noukod.com>
2018-12-23 06:03:50 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,9 +24,9 @@ class ApplicationController < Sinatra::Base
     erb :edit
   end
 
-  get '/recipes/new' do
-    erb :new
-  end
+  # get '/recipes/new' do
+  #   erb :new
+  # end
 
   # post '/recipes' do
   #   recipe = Recipe.create name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time]
